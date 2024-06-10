d99323688e41f917132694cc470ae431e26b80c9
Sanjin Brajević <mynamesafeboot@gmail.com>
2024-06-10 23:08:54 +0200

    Work in progress auth layout

--- stubs/splade/resources/views/auth/login.blade.php
+++ stubs/splade/resources/views/auth/login.blade.php
@@ -1,21 +1,27 @@
 <x-splade-form action="{{ route('login') }}" class="flex flex-col gap-6">
 
     <x-splade-input id="email" type="email" name="email" :label="__('Email')" required autofocus />
+
     <x-splade-input id="password" type="password" name="password" :label="__('Password')" required autocomplete="current-password" />
-    <x-splade-checkbox id="remember_me" name="remember" :label="__('Remember me')" />
 
-    <div class="flex justify-end items-center gap-4">
+    <div class="flex justify-between items-center">
 
-        @if (Route::has('password.request'))
+        <x-splade-checkbox id="remember_me" name="remember" :label="__('Remember me')" />
 
-            <Link href="{{ route('password.request') }}" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
-                {{ __('Forgot your password?') }}
-            </Link>
-
-        @endif
-
-        <x-splade-submit :label="__('Login')" />
+        <Link href="{{ route('password.request') }}" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
+            {{ __('Forgot your password?') }}
+        </Link>
 
     </div>
 
+    <x-splade-submit :label="__('Login')" />
+
 </x-splade-form>
+
+<h1 class="absolute bottom-4 text-center text-lg text-gray-500 dark:text-gray-300">
+
+    Don't have an account yet?
+
+    <Link href="{{ route('register') }}" class="text-blue-400 dark:text-blue-500 hover:text-blue-500 dark:hover:text-blue-600 transition duration-300">Sign up</Link>
+
+</h1>
